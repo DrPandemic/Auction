9b3a76474bb8682918f5fec3ac8c5f2fd141110e
Parasithe <bipbip500@gmail.com>
2014-12-29 15:18:59 -0500

    - depence between db and api

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-var API = require('./lib/wow-api'),
+var DATA = require('./lib/database'),
+  database = new DATA(),
+  API = require('./lib/wow-api'),
   wowApi = new API();
 
 wowApi.query('grim-batol',function(err,body) {
-  if(!err && body) {
+  if(!err && body && body.realm) {
     console.log(body.realm);
+    console.log(body.auctions.auctions.length);
   }
   else {
     console.log(err);

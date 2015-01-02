67871725898f3d115b6eb5065a588520af87cb29
Parasithe <bipbip500@gmail.com>
2015-01-02 16:57:18 -0500

    + retry on error

--- app.js
+++ app.js
@@ -2,20 +2,26 @@ var DATA = require('./lib/database'),
   database = new DATA(),
   API = require('./lib/wow-api'),
   wowApi = new API(),
-  logger = require('./logger');
+  logger = require('./logger'),
+  maxTry = 10;
 
-wowApi.query('grim-batol',function(err,body) {
-  if(!err && body && body.results && body.results.realm) {
-    console.log(body.results.realm);
-    console.log(body.results.auctions.auctions.length);
-    database.insertDump(body.results.auctions.auctions, body.timestamp, function(err, results) {
-      //Doesn't test for error, because it will always have errors due to duplicate
-      logger.log(0, 'Dump was inserted');
-      database.close();
-    });
-  }
-  else {
-    console.log('An error occured ' + err);
-    //TODO : Restart the query
-  }
-});
+function query(server, count, callback) {
+  wowApi.query(server,function(err,body) {
+    if(!err && body && body.results && body.results.realm) {
+      logger.log(0,body.results.realm);
+      logger.log(0,body.results.auctions.auctions.length);
+      database.insertDump(body.results.auctions.auctions, body.timestamp, function(err, results) {
+        //Doesn't test for errors, because it will always have some due to duplicates
+        logger.log(1, 'Dump was inserted');
+        callback(err, results);
+      });
+    }
+    else {
+      console.log('An error occured ' + err);
+      if(count < maxTry)
+        query(server, count + 1, callback);
+      else
+        logger.log(0, 'Gave up trying to get data for : ' + server);
+    }
+  });
+}
